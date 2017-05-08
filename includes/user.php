1c5f29b066398d0d0553342213a42d6dd698d27d
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-05-08 00:47:51 -0400

    before refactor User functions

---
+++ includes/user.php
@@ -0,0 +1,32 @@
+<?php
+  require_once("database.php");
+
+  class User {
+
+  	public static function find_all() {
+  	  global $db;
+
+  	  $result_set = $db->run_query("SELECT * FROM users");
+
+  	  return $result_set;
+  	}
+
+  	public static function find_by_id($id=0) {
+  	  global $db;
+
+  	  $result_set = $db->run_query("SELECT * FROM users WHERE id={$id}");
+  	  $found = $db->fetch_array($result_set);
+  	  return $found;
+  	}
+
+  	public static function find_by_sql($sql="") {
+  	  global $db;
+
+  	  $result_set = $db->run_query($sql);
+
+  	  return $result_set;
+  	}
+
+  }
+
+?>
--- public/index.php
+++ public/index.php
@@ -1,23 +1,12 @@
 <?php 
   require_once("../includes/database.php");
+  require_once("../includes/user.php");
 
   if (isset($db)) { echo "true"; } else { echo "false"; }
 
   echo "<br>";
 
-  echo $db->escape_prep("eet's working");
-
-  echo "<br>";
-
-  // $sql = "INSERT INTO users (id, username, password, first_name, last_name) VALUES (1, 'efitz', 'Password1', 'Eliz', 'Fitz')"; 
-
-  // $result = $db->run_query($sql);
-
-  $sql = "SELECT * FROM users WHERE id=1";
-
-  $result = $db->run_query($sql);
-
-  $found_user = $db->fetch_array($result);
+  $found_user = User::find_by_sql("SELECT * FROM users;");
 
   echo "<br>";
 
