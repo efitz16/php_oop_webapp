1fff249b78217d35ff3853df04bc92b847b36923
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-05-08 01:43:58 -0400

    instantiate users

--- includes/user.php
+++ includes/user.php
@@ -3,6 +3,12 @@
 
   class User {
 
+  	public $id;
+  	public $username;
+  	public $password;
+  	public $first_name;
+  	public $last_name;
+
   	public static function find_all() {
   	  global $db;
 
@@ -26,6 +32,29 @@
   	  $result_set = $db->run_query($sql);
 
   	  return $result_set;
+  	}
+
+  	public function full_name() {
+  	  if(isset($this->first_name) && isset($this->last_name)) {
+  	  	return $this->first_name." ".$this->last_name;
+  	  } else {
+  	  	return "";
+  	  }
+  	}
+
+  	private static function instantiate($record) {
+  	  foreach($record as $attribute->$value) {
+  	  	if($object->has_attribute($attribute)) {
+  	  	  $object->$attribute = $value;
+  	  	}
+  	  }
+  	  return $object;
+  	}
+
+  	private function has_attribute($attribute) {
+  	  $object_vars = get_object_vars($this);
+
+  	  return array_key_exists($attribute, $object_vars);
   	}
 
   }
